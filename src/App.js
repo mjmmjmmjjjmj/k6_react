125ae5918a2c71df267b247a99bd236b11858265
mjmmjmmjjjmj <160682854+mjmmjmmjjjmj@users.noreply.github.com>
2024-04-01 18:10:39 +0900

    Update App_js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MainHeader from './01/MainHeader';
 import Hello from './01/Hello';
 import HelloCss from './02/HelloCss';
-import { LuHome } from "react-icons/lu";
+import { FaHome } from "react-icons/fa";
 import MyDiv1 from './03/03_1/MyDiv1';
 // import MyList from './04/MyList';
 import MyListMain from './04/MyListMain';
@@ -15,6 +15,11 @@ import RefVal from './10/RefVal';
 import RefInput from './10/RefInput';
 import GCard from './11/GCard';
 import Gmain from './11/Gmain';
+import Festival from './12/Festival';
+import Rmain from './13/Rmain';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Frcst from './14/Frcst';
+
 
 
 function App() {
@@ -22,6 +27,7 @@ function App() {
   return ( 
     //JSX 문법 사용
     //react component에서 return이 있어야 하는데 하나만 가능
+    <BrowserRouter>
     <> {/*프레그먼트 태그*/}
     <div className="App"></div>
       {/* <MainHeader /> */}
@@ -41,13 +47,51 @@ function App() {
                         text-xl font-bold text-blue-800
                         bg-slate-200'>
         <div>리액트실습</div>
-        <div><LuHome /></div>
+        <div className='flex justify-end items-center'>
+          <div className='mx-5 p-2 rounded-md 
+                        hover:bg-blue-900 hover:text-white'>
+            <Link to='/lotto2'>로또</Link>
+          </div>
+
+          <div className='mx-5 p-2 rounded-md 
+                        hover:bg-blue-900 hover:text-white'>
+            <Link to='/box'>영화</Link>
+          </div>
+       
+          <div className='mx-5 p-2 rounded-md 
+                        hover:bg-blue-900 hover:text-white'>
+            <Link to='/traffic'>교통</Link>
+          </div>
+          
+          <div className='mx-5 p-2 rounded-md 
+                        hover:bg-blue-900 hover:text-white'>
+            <Link to='/festival'>축제</Link>
+          </div>
+
+          <div className='mx-5 p-2 rounded-md 
+                        hover:bg-blue-900 hover:text-white'>
+            <Link to='/frcst'>예보</Link>
+          </div>
+            
+
+        <div className='text-2xl'> <Link to='/clockforHome' ><FaHome /></Link></div>
+        </div>
 
       </header>
       <main className='grow flex flex-col 
                       justify-center items-center'
                       >
+       <Routes >
+        <Route path='/' element={<MyClock/>} />
+        <Route path='/clockforHome' element={<MyClock/>} />
+        <Route path='/lotto2' element={<Lotto2 />} />
+        <Route path='/box' element={<BoxOfficeTb />} />
+        <Route path='/traffic' element={<TrafficMain />} />
+        <Route path='/festival' element={<Festival />} />
+        <Route path='/frcst' element={<Frcst />} />
+        {/* <Route path='/' element={<Lotto2/>}> */}
         {/* <HelloCss /> */}
+        {/* <Hello/> */}
         {/* <MyDiv1 /> */}
         {/* <MyListMain /> */}
         {/* <Lotto2 /> */}
@@ -57,11 +101,11 @@ function App() {
         {/* <RefVal /> */}
         {/* <BoxOfficeTb /> */}
         {/* <GCard/> */}
-        <Gmain/>
+        {/* <Gmain/> */}
+        {/* <Festival />/ */}
+        {/* <Rmain/> */}
         
-        
-
-
+      </Routes>
       </main>
       <footer className='flex justify-center items-center
                         h-20 bg-slate-800
@@ -73,6 +117,7 @@ function App() {
 
     </div>
     </>
+    </BrowserRouter>
   );
 }
 //화살표 함수로 작성 가능
